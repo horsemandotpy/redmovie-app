e10e6386d0f555637c3864c36a508becb4f53b57
horsemandotpy <chonmegai@outlook.com>
2022-06-23 10:41:03 +0700

    Complete the UI

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "20f8070f00d9374b6dfdf726175e1c63";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
