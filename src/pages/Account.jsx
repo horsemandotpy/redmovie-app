32503f73f8d7a9d84f2bbc4855b918cc5e2b2236
horsemandotpy <chonmegai@outlook.com>
2022-06-25 21:54:44 +0700

    Complete the Basic Function

---
+++ src/pages/Account.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SavedShows from "../components/SavedShows";
+
+const Account = () => {
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c732cb00-be61-4d64-b8c3-99f022e7a123/8581641b-b5eb-4e9c-bbf8-6be15a81f347/VN-en-20220620-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
+};
+
+export default Account;
